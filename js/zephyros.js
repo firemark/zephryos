d93054365541ca8fe06064922b1e9147e656b777
Firemark <marpiechula@gmail.com>
2014-12-08 18:30:54 +0100

    hmm its works, wtf.

--- js/zephyros.js
+++ js/zephyros.js
@@ -6,35 +6,95 @@ var Zephyros = {};
 
 Zephyros.widgets = {};
 Zephyros.fields = {};
+Zephyros.forms = {};
 
 Zephyros.createWidget = function (args) {
+    args = args || {};
     var render = args.render;
+    var getData = args.getData || function () {
+        return this.state.value;
+    };
+    var setData = args.setData || function (value) {
+        this.setState({value: value});
+    };
     var change = args.change || function(event) {
-        this.setState({value: event.target.value});
+        event.preventDefault();
+        this.setData(event.target.value);
+    };
+
+    return React.createClass({
+        getInitialState: function(){
+            return {
+                value: this.props.field.default
+            };
+        },
+        change: change,
+        render: render,
+        getData: getData,
+        setData: setData
+    });
+};
+
+Zephyros.createTemplateForm = function (args) {
+    var formRender = args.formRender;
+    var fieldRender = args.fieldRender;
+    var events = args.events || {};
+
+    var fieldTemplate = React.createClass({
+        render: fieldRender
+    });
+
+    var generateWidgets = function (fields) {
+        return _.map(fields, function (field) {
+            var widgetTemplate = Zephyros.widgets[field.type_field];
+            return React.createElement(
+                widgetTemplate, {ref: field.name, field: field}
+            );
+        });
+    };
+
+    var generateViewField = function(widgets) {
+        return _.map(widgets, function(widget) {
+            return React.createElement(
+                fieldTemplate, {
+                    widget: widget,
+                    field: widget.props.field
+                }
+            );
+        });
     };
 
     return React.createClass({
         getInitialState: function () {
             return {
-                value: args.default,
-                name: args.name,
-                fullname: args.fullname,
-                required: args.required,
-                description: args.description,
-                type: args.type_field
+                errors: {}
             };
         },
-        change: change,
-        render: render
+        events: events,
+        getData: function () {
+            return _.reduce(this.refs, function (obj, widget, key) {
+                obj[key] = widget.getData();
+                return obj;
+            }, {});
+        },
+        setData: function (obj_values) {
+            var refs = this.refs;
+            _.forEach(obj_values, function(value, key) {
+                var widget = refs[key];
+                widget.setData(value);
+            });
+        },
+        render: function () {
+            var widgets = generateWidgets(this.props.fields);
+            var viewFields = generateViewField(widgets);
+            return formRender(viewFields);
+        }
     });
 };
 
-Zephyros.createForm = function (form, fields) {
-    var widgets = [];
-    _.each(fields, function (field) {
-        var widget = Zephyros.widgets[field.type_field];
-        widgets.push(widget(field));
-    });
-
-    return React.createElement("form", null, widgets);
+Zephyros.createForm = function (fields, args) {
+    args = args || {};
+    var templateName = args.template || "default";
+    var template = Zephyros.forms[templateName];
+    return React.createElement(template, {fields: fields});
 };
